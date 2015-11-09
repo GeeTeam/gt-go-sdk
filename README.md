d5897880b405f0183324b07e4a8e3dc20b0c2a77
sdvdxl <sdvdxl@163.com>
2015-11-09 16:08:17 +0800

    add 注释, 添加验证方式

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # geetest
 极验golang sdk
+
+# 使用方法
+- 主要配置是geetest.Config对象.首先我们需要添加自己的公钥和密钥,公钥请配置在geetest.Config.CaptchaId,密钥配置在geetest.Config.PrivateKey.
+- geetest.GeetestLib 是主要的操作方法集合,CheckServerStatus 校验服务器状态,GenerateChallenge 用于生成challenge,Valid 校验验证码是否正确.您可能需要将GeetestLib对象保存的session,在校验验证码是否正确的时候将其取出,调用Valid的方法.如果您不愿意保存该对象,那么可以使用ValidChallenge函数自行校验验证码是否正确.
+
+# 示例
+在samples文件夹中有相关使用示例,`go run server.go`,然后在浏览器访问[localhost:8080](http://localhost:8080)即可访问.
--- geetest.go
+++ geetest.go
@@ -10,20 +10,22 @@ import (
 
 //极验配置
 type geetestConfig struct {
-	CaptchaId       string //应用id
-	PrivateKey      string //应用密钥
-	IsHttps         bool   //是否是https
-	Debug           bool   //是否开启debug模式
-	ServerStatusUrl string
-	RegisterUrl     string
+	CaptchaId       string //应用captcha id 必填
+	PrivateKey      string //应用密钥  必填
+	IsHttps         bool   //是否是https 可选,如果是https,则设置为true, 暂未实现
+	Debug           bool   //是否开启debug模式 暂未实现
+	ServerStatusUrl string //服务器状态校验url, 可选
+	RegisterUrl     string //注册获取challenge地址 可选
 }
 
+// 初始化基本配置
 func init() {
 	Config.ServerStatusUrl = "http://api.geetest.com/check_status.php"
 	Config.RegisterUrl = "http://api.geetest.com/register.php?gt="
 
 }
 
+// 极验配置项
 var Config geetestConfig
 
 type GeeTestLib struct {
@@ -32,6 +34,7 @@ type GeeTestLib struct {
 
 
 // 校验服务器是否正常
+// 如果服务器正常,且返回正确状态(ok),则返回true,否则false
 func (self GeeTestLib)CheckServerStatus() bool {
 	resp, err := http.Get(Config.ServerStatusUrl)
 
@@ -63,25 +66,42 @@ func (self *GeeTestLib)GenerateChallenge() (string, error) {
 		return "", err
 	}
 
+	defer resp.Body.Close()
+
 	self.Challenge = getResponseResult(resp)
 	return self.Challenge, nil
 }
 
-//校验
+// 校验验证码是否正确
+// 由于极验challenge传回服务器会自动在后面加上2位随机字母,所以需要传回后台
+// 传回极验服务器的challenge=challenge+2字母,所以是34位,否则校验失败
+// 根据极验服务器加密方式,将从极验服务器得到的加密码和后台加密码对比,
+// 如果相同则校验成功,返回true, 否则false
+// challenge 前端传过来的challenge, 默认是 geetest_challenge 参数
+// secCode 前端传过来的加密后的值,默认是 geetest_validate 参数
 func (self GeeTestLib)Valid(challenge, secCode string) bool {
-	if len(challenge) != 34 || challenge[:32] != self.Challenge {
+	return ValidChallenge(challenge, self.Challenge, secCode)
+}
+
+// 用于校验验证码, 和Valid方法功能相同,但是允许自行传入之前生成的challenge进行校验
+// frontChallenge 前端传过来的34位challenge
+// backChallenge 后台生成的challenge
+// secCode 前台传过来的加密校验码
+func ValidChallenge(frontChallenge, backChallenge ,secCode string) bool {
+	if len(frontChallenge) != 34 || frontChallenge[:32] != backChallenge {
 		return false
 	}
 
-	return strings.EqualFold(md5Encode(Config.PrivateKey + "geetest" + challenge), secCode)
+	return strings.EqualFold(md5sum(Config.PrivateKey + "geetest" + frontChallenge), secCode)
 }
 
-func md5Encode(text string) string {
+// 计算字符串的md5
+func md5sum(text string) string {
 	result := md5.Sum([]byte(text))
 	return hex.EncodeToString(result[:])
 }
 
-
+// 处理http返回内容
 func getResponseResult(resp *http.Response) string {
 	if resp == nil {
 		return ""
@@ -94,3 +114,4 @@ func getResponseResult(resp *http.Response) string {
 	return string(bodyBytes)
 
 }
+
